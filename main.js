0166a068bf4b84a014cac03ec88d293c80246aba
slpkid <153894144+slpkid@users.noreply.github.com>
2024-03-02 10:02:12 -0800

    Feat: add operate function for resolving equations

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let numberOne = 10
+let numberTwo = 10
+let operator = "*"
+
 function add (a,b) {
 	return a + b
 }
@@ -10,3 +14,17 @@ function multiply (a,b) {
 function divide (a,b) {
 	return a / b
 }
+function operate(num1,num2,op) {
+    switch(op) {
+        case op = "+":
+            return add(num1, num2)
+        case op = "-":
+            return subtract(num1,num2)
+        case op = "*":
+            return multiply(num1,num2)
+        case op = "/":
+            return divide(num1,num2)
+    }
+}
+
+console.log(operate(numberOne,numberTwo,operator))
