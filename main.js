e7de59627da4d57c6b8412a4c32cf87fea141991
slpkid <153894144+slpkid@users.noreply.github.com>
2024-03-02 14:14:38 -0800

    Feat: Operators can be used to do math

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 let numberOne = 0;
 let numberTwo;
 let operator;
+let phase = 1;
+clearNum = false;
 let screenValue = document.querySelector(".screen");
 const numButtons = document.querySelectorAll(".numButton");
 const clearButton = document.querySelector(".clear");
+const opButtons = document.querySelectorAll(".opButton")
 
 function add (a,b) {
 	return a + b
@@ -31,17 +34,40 @@ function operate(num1,num2,op) {
 }
 
 clearButton.addEventListener("click", () => {
-    screenValue.textContent = 0
+    phase = 1;
+    screenValue.textContent = 0;
+    numberOne = 0;
+    operator = undefined;
+    numberTwo = undefined;
 })
 
 numButtons.forEach(button => button.addEventListener("click", () => {
+    if (phase === 2 && clearNum === true) {
+        screenValue.textContent = "";
+        clearNum = false;
+    }
     if (button.textContent === "0" && screenValue.textContent === "0") {
-        exit();
+        return
     }
     if (screenValue.textContent === "0"){
         screenValue.textContent = ""
     }
     screenValue.textContent += button.textContent;
+}))
+
+opButtons.forEach(button => button.addEventListener("click", () => {
+    if (phase === 1) {
+        numberOne = screenValue.textContent;
+        operator = button.textContent;
+        phase = 2;
+        clearNum = true;
+        return;
+    }
+    if (phase === 2) {
+        numberTwo = screenValue.textContent;
+        screenValue.textContent = operate(numberOne,numberTwo,operator);
+        phase = 1;
+    }
 }))
 
 //console.log(operate(numberOne,numberTwo,operator));
