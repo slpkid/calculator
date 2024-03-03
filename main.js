cc5abc9b20863237af3e30b9b0c3b32c1ad1d947
slpkid <153894144+slpkid@users.noreply.github.com>
2024-03-03 09:44:36 -0800

    Separate functions from event listeners

--- main.js
+++ main.js
@@ -34,15 +34,23 @@ function operate(num1,num2,op) {
     }
 }
 
-//reinitializes all values. unsure if logic based on undefineds is poor quality code or not...
-clearButton.addEventListener("click", () => {
+function divideByZero() {    
+    screenValue.textContent = "kappa"
+    clearNum = true;
+    operator = undefined;
+    numberOne = undefined;
+    numberTwo = undefined;
+}
+
+function clearButtonPress() {
+    //reinitializes all values. unsure if logic based on undefineds is poor quality code or not...
     screenValue.textContent = 0;
     operator = undefined;
     numberOne = undefined;
     numberTwo = undefined;
-})
+}
 
-numButtons.forEach(button => button.addEventListener("click", () => {
+function numberPress () {
     // will clear the display after pressing an operator or equals button for clarity
     if (clearNum === true) {
         screenValue.textContent = "";
@@ -52,21 +60,54 @@ numButtons.forEach(button => button.addEventListener("click", () => {
     if (button.textContent === "0" && screenValue.textContent === "0") {
         return
     }
+    // clears the zero when other flags fail 
     if (screenValue.textContent === "0"){
         screenValue.textContent = ""
     }
     screenValue.textContent += button.textContent;
-}))
-
-function divideByZero() {    
-    screenValue.textContent = "kappa"
-    clearNum = true;
-    operator = undefined;
-    numberOne = undefined;
-    numberTwo = undefined;
 }
 
-opButtons.forEach(button => button.addEventListener("click", () => {
+function equalsPress() {
+    // when no operator is present, flag clearNum and do nothing.
+    if (operator === undefined) {
+        clearNum = true
+        return;
+    }
+    // otherwise it operates and stores numberTwo to allow for iterable calculations
+    numberTwo = Number(screenValue.textContent);
+    //check if it's zero, end function if yes.
+    if (numberTwo === 0 && operator === "/") {
+        divideByZero();
+        return;
+    }
+    screenValue.textContent = operate(numberOne,numberTwo,operator).toString();
+    clearNum = true;
+    return;
+}
+
+function roundNum(num) {
+    return Number(num.toString().slice(0,15))
+}
+
+function decimalPress() {
+    let num = screenValue.textContent
+    processedNum = num.replace(/[^.]/gm, "");
+    // Reduces onscreen text to a string containing nothing or a single decimal.
+    // check if clearNum is true, then print "0." to the screen
+    if (processedNum && clearNum === true) {
+        screenValue.textContent = "0."
+        clearNum = false;
+        return;
+        // check for clearNum, and prevents multiple decimal presses if it's false
+    } else if (processedNum && clearNum === false) {
+        return
+    }
+    // if it passes both checks, then add a decimal.
+    screenValue.textContent += "."
+    return;
+}
+
+function operatorPress() {
     // this fires to store the first number to be operated on to prevent an empty calcuation
     if (numberOne === undefined) {
         numberOne = Number(screenValue.textContent);
@@ -77,6 +118,7 @@ opButtons.forEach(button => button.addEventListener("click", () => {
     // operate upon stored values and returns the result
     if (numberOne) {
         numberTwo = Number(screenValue.textContent);
+        //check if it's zero, end function if yes.
         if (numberTwo === 0 && operator === "/") {
             divideByZero();
             return;
@@ -88,49 +130,24 @@ opButtons.forEach(button => button.addEventListener("click", () => {
         clearNum = true;
         return;
     }
-}))
-
-enterButton.addEventListener("click", () => {
-    // when no operator is present, flag clearNum and do nothing.
-    if (operator === undefined) {
-        clearNum = true
-        return;
-    }
-    // otherwise it operates and stores numberTwo to allow for iterable calculations
-    numberTwo = Number(screenValue.textContent);
-    if (numberTwo === 0 && operator === "/") {
-        divideByZero();
-        return;
-    }
-    let result = operate(numberOne,numberTwo,operator).toString();
-    if (result.length > 15) {
-        screenValue.textContent = roundNum(result)
-    } else {
-    screenValue.textContent = result
-    }
-    clearNum = true;
-    return;
-})
-
-function roundNum(num) {
-    return Number(num.toString().slice(0,15))
 }
 
+enterButton.addEventListener("click", () => {
+    equalsPress()
+})
+
 decimalButton.addEventListener("click", () => {
-    let num = screenValue.textContent
-    processedNum = num.replace(/[^.]/gm,"");
-    // Reduces onscreen text to a string containing nothing or a single decimal.
-    // check if clearNum is true, then print "0." to the screen
-    if (processedNum && clearNum === true) {
-        screenValue.textContent = "0."
-        clearNum = false;
-        return;
-    // check for clearNum, and prevents multiple decimal presses if it's false
-    } else if (processedNum && clearNum === false) {
-        return
-    }
-    // if it passes both checks, then add a decimal.
-    screenValue.textContent += "."
-    return;
-    }
-)
+    decimalPress()
+})
+
+opButtons.forEach(button => button.addEventListener("click", () => {
+    operatorPress()
+}))
+
+numButtons.forEach(button => button.addEventListener("click", () => {
+    numberPress()
+}))
+
+clearButton.addEventListener("click", () => {
+    clearButtonPress()
+})
